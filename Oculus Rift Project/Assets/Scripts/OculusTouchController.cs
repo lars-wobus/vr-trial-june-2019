689f04abb82845a82869a571e7de7582363bfa5f
Lars Wobus <lars.wobus@web.de>
2019-06-09 12:05:19 +0200

    Fix controller size and control position via script

---
+++ Oculus Rift Project/Assets/Scripts/OculusTouchController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class OculusTouchController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform touchControllerLeft;
+
+    [SerializeField]
+    private Transform touchControllerRight;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.GetControllerPositionTracked(OVRInput.Controller.LTouch))
+            touchControllerLeft.localPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.LTouch);
+
+        if (OVRInput.GetControllerPositionTracked(OVRInput.Controller.RTouch))
+            touchControllerRight.localPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
+    }
+}
