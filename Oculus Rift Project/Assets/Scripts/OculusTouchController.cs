3dd86bafee5bd841f0438ea5f43e4790827f225c
Lars Wobus <lars.wobus@web.de>
2019-06-09 12:46:12 +0200

    Log message on key press

--- Oculus Rift Project/Assets/Scripts/OculusTouchController.cs
+++ Oculus Rift Project/Assets/Scripts/OculusTouchController.cs
@@ -22,5 +22,50 @@ public class OculusTouchController : MonoBehaviour
             touchControllerRight.localPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
             touchControllerRight.localRotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch);
         }
+
+        if (OVRInput.GetDown(OVRInput.Button.One, OVRInput.Controller.LTouch))
+        {
+            Debug.Log("1");
+        }
+
+        if (OVRInput.GetDown(OVRInput.Button.Two, OVRInput.Controller.LTouch))
+        {
+            Debug.Log("2");
+        }
+
+        if (OVRInput.GetDown(OVRInput.Button.One, OVRInput.Controller.RTouch))
+        {
+            Debug.Log("3");
+        }
+
+        if (OVRInput.GetDown(OVRInput.Button.Two, OVRInput.Controller.RTouch))
+        {
+            Debug.Log("4");
+        }
+
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.LTouch))
+        {
+            Debug.Log("PrimaryIndexTrigger");
+        }
+
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch))
+        {
+            Debug.Log("SecondaryIndexTrigger");
+        }
+
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger, OVRInput.Controller.LTouch))
+        {
+            Debug.Log("PrimaryHandTrigger");
+        }
+
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger, OVRInput.Controller.RTouch))
+        {
+            Debug.Log("SecondaryHandTrigger");
+        }
+
+        if (OVRInput.GetDown(OVRInput.Button.Start))
+        {
+            Debug.Log("Start");
+        }
     }
 }
