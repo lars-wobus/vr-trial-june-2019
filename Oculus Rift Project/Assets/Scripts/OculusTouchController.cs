a07e5b4cd2fe9ae9bfe4b7e791ba75063b07a1f3
Lars Wobus <lars.wobus@web.de>
2019-06-09 12:11:06 +0200

    Supporting touch controller rotation

--- Oculus Rift Project/Assets/Scripts/OculusTouchController.cs
+++ Oculus Rift Project/Assets/Scripts/OculusTouchController.cs
@@ -8,13 +8,19 @@ public class OculusTouchController : MonoBehaviour
     [SerializeField]
     private Transform touchControllerRight;
 
-    // Update is called once per frame
     void Update()
     {
         if (OVRInput.GetControllerPositionTracked(OVRInput.Controller.LTouch))
+        {
             touchControllerLeft.localPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.LTouch);
+            touchControllerLeft.localRotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.LTouch);
+        }
+            
 
         if (OVRInput.GetControllerPositionTracked(OVRInput.Controller.RTouch))
+        {
             touchControllerRight.localPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
+            touchControllerRight.localRotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch);
+        }
     }
 }
