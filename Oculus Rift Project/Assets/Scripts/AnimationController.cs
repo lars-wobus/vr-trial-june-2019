08d55b813458830ac8ba5b198b3b2c8379ca7f48
Lars Wobus <lars.wobus@web.de>
2019-06-10 10:01:50 +0200

    Test walk animation in test scene

---
+++ Oculus Rift Project/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform character;
+
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = character.GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            animator.SetBool("Walk", true);
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            animator.SetBool("Walk", false);
+        }
+    }
+}
