2dd10c1be69220ebdadffbdaab56a17f36dcef4c
Lars Wobus <lars.wobus@web.de>
2019-06-10 23:15:46 +0200

    Active VR in all scenes

---
+++ Oculus Rift Project/Assets/Scripts/PressToContinue.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class PressToContinue : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent customEvent;
+
+    private void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.Start))
+        {
+            Debug.Log("Start");
+            customEvent?.Invoke();
+        }
+    }
+}
