7fae7ce5d2c35fabbd1b9d3d4d5dc64ccbee33cb
Lars Wobus <lars.wobus@web.de>
2019-06-10 16:12:03 +0200

    Start making some audio assets

---
+++ Oculus Rift Project/Assets/Scripts/AudioControl.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Linq;
+
+public class AudioControl : MonoBehaviour
+{
+    [SerializeField]
+    private AudioData[] data;
+
+    public AudioData FindAudioSourceByName(string name)
+    {
+        return data.First(element => element.Name == name);
+    }
+}
---
+++ Oculus Rift Project/Assets/Scripts/AudioData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System;
+
+[Serializable]
+public class AudioData
+{
+    [SerializeField]
+    private string name;
+
+    [SerializeField]
+    private AudioSource audioSource;
+
+    public string Name => name;
+    public AudioSource AudioSource => audioSource;
+}
